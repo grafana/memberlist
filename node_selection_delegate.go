5753b829cbd2d932b156548f92a9da7cbedcdc6f
Marco Pracucci <marco@pracucci.com>
2025-10-24 10:31:13 +0200

    Don't use NodeSelectionDelegate for indirect probes, given that direct probes don't use it neither
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- node_selection_delegate.go
+++ node_selection_delegate.go
@@ -3,8 +3,8 @@
 package memberlist
 
 // NodeSelectionDelegate is an optional delegate that can be used to filter and prioritize
-// nodes for gossip, probing, and push/pull operations. This allows implementing custom
-// routing logic, such as zone-aware or rack-aware gossiping.
+// nodes for gossip, and push/pull operations. This allows implementing custom routing logic,
+// such as zone-aware or rack-aware gossiping.
 type NodeSelectionDelegate interface {
 	// SelectNode is called for each node to determine:
 	// - selected: whether the node should be included in the selection pool
